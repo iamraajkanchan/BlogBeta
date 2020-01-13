5c88ac498bcbd36ca0caf20304c3c74332efad59
Raaj Kanchan <iamraajkanchan@gmail.com>
2020-01-13 10:11:28 +0530

    13th Jan 2020 Update
    Fixed navigation-stack

---
+++ src/screens/IndexSearch.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const IndexSearch = () => {
+    return (
+        <View>
+            <Text>Index Search</Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+
+})
+
+export default IndexSearch;
